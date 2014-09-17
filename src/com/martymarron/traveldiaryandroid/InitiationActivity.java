eed2e60e8d23f119beda983d28ee037ccb0dad3e
x-masashik <x-masashik@JPC20139916.jp.sony.com>
2014-09-17 18:28:51 +0900

    Add Initiation Activity

---
+++ src/com/martymarron/traveldiaryandroid/InitiationActivity.java
@@ -0,0 +1,7 @@
+package com.martymarron.traveldiaryandroid;
+
+import android.support.v7.app.ActionBarActivity;
+
+public class InitiationActivity extends ActionBarActivity {
+
+}
---
+++ src/com/martymarron/traveldiaryandroid/InitiationFragment.java
@@ -0,0 +1,7 @@
+package com.martymarron.traveldiaryandroid;
+
+import android.support.v4.app.Fragment;
+
+public class InitiationFragment extends Fragment {
+
+}
